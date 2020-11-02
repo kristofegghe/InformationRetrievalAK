733abac4e0aee9ec694847a303331707a87b02df
kristofegghe <s0172975@ad.ua.ac.be>
2020-11-02 18:31:49 +0100

    lxml iterparse gebruiken zodat mijn pc niet opblaast als ik probeer de stackoverflow te parsen. source komt later nog

--- lucienneee.py
+++ lucienneee.py
@@ -14,67 +14,17 @@ from org.apache.lucene.store import FSDirectory,SimpleFSDirectory,MMapDirectory
 from org.apache.lucene.util import Version
 from org.apache.lucene.queryparser.classic import QueryParser
 from xml.etree import cElementTree as ElementTree
+from lxml import etree
 
 
 
 # https://code.activestate.com/recipes/410469-xml-as-dictionary/
-
-class XmlListConfig(list):
-    def __init__(self, aList):
-        for element in aList:
-            if element:
-                # treat like dict
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    self.append(XmlDictConfig(element))
-                # treat like list
-                elif element[0].tag == element[1].tag:
-                    self.append(XmlListConfig(element))
-            elif element.text:
-                text = element.text.strip()
-                if text:
-                    self.append(text)
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
 
-class XmlDictConfig(dict):
-
-    def __init__(self, parent_element):
-        if parent_element.items():
-            self.update(dict(parent_element.items()))
-        for element in parent_element:
-            if element:
-                # treat like dict - we assume that if the first two tags
-                # in a series are different, then they are all different.
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    aDict = XmlDictConfig(element)
-                # treat like list - we assume that if the first two tags
-                # in a series are the same, then the rest are the same.
-                else:
-                    # here, we put the list in dictionary; the key is the
-                    # tag name the list elements all share in common, and
-                    # the value is the list itself
-                    aDict = {element[0].tag: XmlListConfig(element)}
-                # if the tag has attributes, add those to the dict
-                if element.items():
-                    aDict.update(dict(element.items()))
-                self.update({element.tag: aDict})
-            # this assumes that if you've got an attribute in a tag,
-            # you won't be having any text. This may or may not be a
-            # good idea -- time will tell. It works for the way we are
-            # currently doing XML configuration files...
-            elif element.items():
-                self.update({element.tag: dict(element.items())})
-            # finally, if there are no child tags and no attributes, extract
-            # the text
-            else:
-                self.update({element.tag: element.text})
-
-
-tree = ElementTree.parse('../../../andy/Desktop/InformationRetrieval/pom.xml')
-root = tree.getroot()
-lijst = []
-for i in root:
-    lijst.append(XmlDictConfig(i))
-print("ok")
+infile="../stackoverflow.com-Posts/Posts.xml"
+context = etree.iterparse(infile)
+counter=0
 
 
 
@@ -83,27 +33,27 @@ print("ok")
 
 
 
-
+#
 # infile="smallTest.xml"
 # context = etree.iterparse(infile, events=('end',), tag='title')
 #
-# for event, elem in context:
-#     print('%s\n' % elem.text.encode('utf-8'))
-# doc = etree.parse('smallTest.xml')
-# # obj = untangle.parse('smallTest.xml')
-# print(etree.tostring(doc))
-# print("ok")
+# # for event, elem in context:
+# #     print('%s\n' % elem.text.encode('utf-8'))
+# # doc = etree.parse('smallTest.xml')
+# # # obj = untangle.parse('smallTest.xml')
+# # print(etree.tostring(doc))
+# # print("ok")
+# #
+# # for i in doc.getroot():
+# #     j =  etree.tostring(i)
+# #     dict_str = j.decode("UTF-8")
 #
-# for i in doc.getroot():
-#     j =  etree.tostring(i)
-#     dict_str = j.decode("UTF-8")
-
-# print("ok")
-lucene.initVM(vmargs=['-Djava.awt.headless=true'])
-# Basic tokenizer example.
-test = "This is euh how we do it."
-dir= "directori/"
-
+# # print("ok")
+# lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+# # Basic tokenizer example.
+# test = "This is euh how we do it."
+# dir= "directori/"
+#
 
 def openStore():
     return SimpleFSDirectory(Paths.get("index/"))
@@ -118,21 +68,30 @@ def getWriter(store, analyzer=None, create=False):
     writer = IndexWriter(store, config)
 
     return writer
-def indexiets(docList):
+def indexiets(context):
     store=openStore()
     writer=None
     try:
         analyzer =StandardAnalyzer()
         writer = getWriter(store, analyzer, True)
-        for document in docList:
+        counter=0
+        for event,elem in context:
+            if(counter>1500000):
+                break
+            print(counter)
+            counter+=1
             doc = Document()
-            for key in document:
-                doc.add(Field(key, document[key],
-                              TextField.TYPE_STORED))
-
+            for key in elem.attrib:
+                doc.add(Field(key, elem.attrib[key],
+                                  TextField.TYPE_STORED))
             writer.addDocument(doc)
+            elem.clear()
+            for ancestor in elem.xpath('ancestor-or-self::*'):
+                while ancestor.getprevious() is not None:
+                    del ancestor.getparent()[0]
 
     finally:
+        del context
         writer.close()
 
 def query(searchTerm):
@@ -153,6 +112,5 @@ def query(searchTerm):
             rank+=1
     finally:
         reader.close()
-
-indexiets(lijst)
-query("decimal")
+indexiets(context)
+# query("Javascript")
